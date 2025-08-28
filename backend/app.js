aa7e81f92d1dcdd77573e670192f704918df0398
Nikita <nekit9401@yandex.ru>
2025-08-28 23:58:02 +0300

    fix path-to-regexp

--- backend/app.js
+++ backend/app.js
@@ -9,15 +9,15 @@ const routes = require('./routes');
 const port = 3001;
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.resolve('..', 'frontend', 'dist')));
 
 app.use(cookieParser());
 app.use(express.json());
 
 app.use('/api', routes);
 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+app.get('/{*path}', (req, res) => {
+	res.sendFile(path.resolve('..', 'frontend', 'dist', 'index.html'));
 });
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => {
